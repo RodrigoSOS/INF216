8ee292786c94cd8e0b49e219611488e75a06aed3
RodrigoSOS <doxrigor@gmail.com>
2022-03-12 00:25:06 -0300

    Update iso.js

--- inf216/pratica8/iso.js
+++ inf216/pratica8/iso.js
@@ -171,7 +171,7 @@ class Example extends Phaser.Scene
 
         // graphics.lineStyle(2, 0xffd900, 1);
 
-        graphics.beginFill(0xFF0000, 1);
+        //graphics.beginFill(0xFF0000, 1);
         graphics.drawCircle(300, 300, 100);
     }
 
